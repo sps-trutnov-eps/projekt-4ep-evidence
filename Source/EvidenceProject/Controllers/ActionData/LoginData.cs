9d472c292053b842c744343618532c4a534090f1
Daniel Samek <samek@spstrutnov.cz>
2022-09-20 09:18:50 +0200

    Oprava chyby
    System.InvalidOperationException: Expiration cannot be set for the cookie defined by SessionOptions

--- Source/EvidenceProject/Controllers/ActionData/LoginData.cs
+++ Source/EvidenceProject/Controllers/ActionData/LoginData.cs
@@ -1,7 +1,6 @@
 namespace EvidenceProject.Controllers.RequestClasses;
 public class LoginData 
 {
-    // TODO Pouzit modely, nebo podedit
     public string? username { get; set; }
     public string? password { get; set; }
 }
