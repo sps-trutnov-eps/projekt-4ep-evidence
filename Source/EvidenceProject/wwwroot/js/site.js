6a387189ba48a5edd6761ae5de04ecc752f69c43
Michaela Špetlová <spetlova@spstrutnov.cz>
2022-09-27 14:20:02 +0200

    názvy souborů se ukazují

--- Source/EvidenceProject/wwwroot/js/site.js
+++ Source/EvidenceProject/wwwroot/js/site.js
@@ -1,4 +1,13 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+console.log("workin");
+const fileSelector = document.getElementById('photo');
+fileSelector.addEventListener('change', (event) => {
+    const fileList = event.target.files;
+    console.log(fileList);
+    document.getElementById("nazvy").innerHTML = "";
+    for (let i = 0; i < fileList.length; i++) {
+        /*console.log(fileList[i].name);*/
+        ted = document.getElementById("nazvy").innerText;
+        document.getElementById("nazvy").innerHTML = ted + " " + fileList[i].name;
 
-// Write your JavaScript code.
+    }
+});
