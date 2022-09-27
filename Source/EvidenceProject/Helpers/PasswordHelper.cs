0f144b287a4f700e37da9465b780c95fc878392f
Marek Veselý <veselym@spstrutnov.cz>
2022-09-27 14:46:55 +0200

    PasswordHelper Tests

--- Source/EvidenceProject/Helpers/PasswordHelper.cs
+++ Source/EvidenceProject/Helpers/PasswordHelper.cs
@@ -9,11 +9,12 @@ public class PasswordHelper {
     static public string CreateHash(string input)
     {
         var argon = new Argon2id(Encoding.UTF8.GetBytes(input));
+        argon.Salt = Encoding.UTF8.GetBytes("00000000");
         argon.DegreeOfParallelism = 8;
         argon.Iterations = 4;
         argon.MemorySize = 1024;
 
-        return Convert.ToBase64String(argon.GetBytes(16));
+        return Convert.ToHexString(argon.GetBytes(16));
     }
 
     static public bool VerifyHash(string input, string hash)
