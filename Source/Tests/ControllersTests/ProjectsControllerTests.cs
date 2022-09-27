54ecfcf5b651961ce8ee116ccb659f03635f15af
Daniel Samek <samek@spstrutnov.cz>
2022-09-27 14:56:50 +0200

    Oprava testů?

--- Source/Tests/ControllersTests/ProjectsControllerTests.cs
+++ Source/Tests/ControllersTests/ProjectsControllerTests.cs
@@ -1,9 +1,12 @@
 using EvidenceProject.Controllers;
+using EvidenceProject.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Tests.ControllersTests
 {
     public class ProjectControllerTests
     {
+        private ProjectController Controller = new ProjectController(new ProjectContext(new DbContextOptions<ProjectContext>()));
         [Test]
         public void Test1() => Assert.Pass();
     }
