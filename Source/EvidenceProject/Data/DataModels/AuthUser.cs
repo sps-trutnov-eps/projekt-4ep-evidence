7daf842c94e871c5b8336771506e294ab8be0889
Kettner Lubos <kettner@spstrutnov.cz>
2022-10-23 17:15:40 +0200

    Pomocné metody v UserModelu

--- Source/EvidenceProject/Data/DataModels/AuthUser.cs
+++ Source/EvidenceProject/Data/DataModels/AuthUser.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
+using bcrypt = BCrypt.Net.BCrypt;
 
 namespace EvidenceProject.Data.DataModels;
 
@@ -16,7 +17,73 @@ public class User
 public class AuthUser : User
 {
     [Required] [StringLength(25)] public string? username { init; get; }
-    public string? password { set; get; }
+    [Required] public string? password { set; get; }
     public bool? globalAdmin { get; init; }
+    [Required] public string? id_key { get; private set; }
+
+    private bool notHashedPass = true;
+
+    public AuthUser(ProjectContext? context = null)
+    {
+        GenerateIdKey(context);
+    }
+
+    public AuthUser(string login, string pass, string fullname = "", string studyfield = "", string contact = "", bool admin = false, ProjectContext? context = null)
+    {
+        username = login;
+        HashPassword(pass);
+        GenerateIdKey(context);
+
+        fullName = fullname;
+        studyField = studyfield;
+        contactDetails = contact;
+
+        globalAdmin = admin;
+    }
+
+    ~AuthUser()
+    {
+        if (notHashedPass)
+        {
+            HashPassword();
+        }
+    }
+
+    public void HashPassword(string? pass = null)
+    {
+        password = bcrypt.HashPassword(pass ?? password);
+
+        notHashedPass = false;
+    }
+
+    public bool VerifyPassword(string passToCompare)
+    {
+        if (bcrypt.Verify(passToCompare, password)) { return true; }
+
+        return false;
+    }
+
+    public void GenerateIdKey(ProjectContext? context = null, int keyLength = 32)
+    {
+        id_key = ReturnIdKey(keyLength, context);
+    }
+
+    private string ReturnIdKey(int keyLength, ProjectContext? context = null)
+    {
+        string key = "";
+
+        do
+        {
+            key = "";
+            Random rand = new Random();
+            for (int i = 0; i < keyLength; i++)
+            {
+                int cislo = rand.Next(48, 122);
+                key += Convert.ToChar(cislo).ToString();
+            }
+        } while (context?.globalUsers?.Where(a => a.id_key == key).FirstOrDefault() != null);
+
+        return key;
+    }
 }
 
