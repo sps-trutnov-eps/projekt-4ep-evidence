fafbf5b909db6b2f74f712b6614975d1ac2e7625
KrutisGod <krutis@spstrutnov.cz>
2022-10-23 17:40:10 +0200

    nefunkcni část kodu

--- Source/EvidenceProject/wwwroot/js/site.js
+++ Source/EvidenceProject/wwwroot/js/site.js
@@ -1,13 +1,14 @@
 console.log("workin");
 const fileSelector = document.getElementById('photo');
-fileSelector.addEventListener('change', (event) => {
-    const fileList = event.target.files;
-    console.log(fileList);
-    document.getElementById("nazvy").innerHTML = "";
-    for (let i = 0; i < fileList.length; i++) {
-        /*console.log(fileList[i].name);*/
-        ted = document.getElementById("nazvy").innerText;
-        document.getElementById("nazvy").innerHTML = ted + " " + fileList[i].name;
+//fileSelector.addEventListener('change', (event) => {
+//    const fileList = event.target.files;
+//    console.log(fileList);
+//    document.getElementById("nazvy").innerHTML = "";
+//    for (let i = 0; i < fileList.length; i++) {
+//        /*console.log(fileList[i].name);*/
+//        ted = document.getElementById("nazvy").innerText;
+//        document.getElementById("nazvy").innerHTML = ted + " " + fileList[i].name;
 
-    }
-});
+//    }
+//});
+
