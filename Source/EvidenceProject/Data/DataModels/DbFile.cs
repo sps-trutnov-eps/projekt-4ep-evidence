631acf8440c4385550d0053568cda7894550f332
Jakub Suchánek <suchys.jakub@gmail.com>
2022-10-23 12:30:30 +0200

    Info Source

--- Source/EvidenceProject/Data/DataModels/DbFile.cs
+++ Source/EvidenceProject/Data/DataModels/DbFile.cs
@@ -2,8 +2,10 @@
 
 namespace EvidenceProject.Data.DataModels;
 
-public class DbFile // https://stackoverflow.com/questions/2579373/saving-any-file-to-in-the-database-just-convert-it-to-a-byte-array
+public class DbFile
 {
+    //Any file - https://stackoverflow.com/questions/2579373/saving-any-file-to-in-the-database-just-convert-it-to-a-byte-array
+    //Image - http://www.binaryintellect.net/articles/2f55345c-1fcb-4262-89f4-c4319f95c5bd.aspx
     [Key]
     public Guid id { get; set; }
     [Required]
