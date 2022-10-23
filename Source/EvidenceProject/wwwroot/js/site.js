92a4920c37fcfb57c88e9be98f493a2733f205de
Pavel Amler <amler@spstrutnov.cz>
2022-10-23 18:19:56 +0200

    Plynulý přechod mezi stránkami

--- Source/EvidenceProject/wwwroot/js/site.js
+++ Source/EvidenceProject/wwwroot/js/site.js
@@ -1,4 +1,37 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    PlynulyPrechodMeziStrankami();
+});
 
-// Write your JavaScript code.
+function PlynulyPrechodMeziStrankami(){
+    history.replaceState({"html":$("html").prop("outerHTML")}, "", $(location).attr("pathname"));
+
+    $(document).on("click", ".odkaz", function () {
+        let link = $(this).attr('href');
+
+        $("main").empty();
+        $("main").html("<div>načítání dat...</div>");
+    
+        $.ajax({
+            type : "GET",
+            url : link,
+            dataType: "html",
+            success : function(html){
+                let stranka = $($.parseHTML(html));
+                $("main").replaceWith(stranka.filter("main"));
+                $("title").replaceWith(stranka.filter("title"));
+                history.pushState({"html":html}, "", link);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                $("main").html(`<div>${jqXHR.status} ${errorThrown}</div>`);
+            }
+        });
+    
+        return false;
+    });
+}
+window.onpopstate = function(e){
+    if (e.state == null) return;
+    let stranka = $($.parseHTML(e.state.html));
+    $("main").replaceWith(stranka.filter("main"));
+    $("title").replaceWith(stranka.filter("title"));
+};
