6a7669fe29064ffd443b9fcc0bf753709ad1255d
Kettner Lubos <kettner@spstrutnov.cz>
2022-10-30 12:27:13 +0100

    Dokumentace v kódu v2.
    Dokumentace číselníků a projektu.

--- Source/EvidenceProject/Data/DataModels/DialCode.cs
+++ Source/EvidenceProject/Data/DataModels/DialCode.cs
@@ -7,22 +7,34 @@ namespace EvidenceProject.Data.DataModels
 {
     public class DialCode
     {
+        /// <summary> Unikátní identifikátor záznamu.
+        /// </summary>
         [Key]  
         public int id { get; set; }
 
+        /// <summary> Název/obsah číselníkového záznamu - max 50 znaků.
+        /// </summary>
         [Required]
         [StringLength(50)]
         public string? name { get; set; }
 
+        /// <summary> Barevnost vizualizace záznamu.
+        /// </summary>
         [NotMapped]
         public Color? color { get => Color.FromArgb(_color); set => _color = value?.ToArgb() ?? 0; }
 
+        /// <summary> Číselný záznam barvy pro uložení do DB.
+        /// </summary>
         [Required]
-        public int _color { get; set; }
+        private int _color { get; set; }
 
+        /// <summary> Popis záznamu číselníku. Max 200 znaků.
+        /// </summary>
         [StringLength(200)]
         public string? description { get; set; }
 
+        /// <summary> Kategorie, číselníků do které záznam zapadá.
+        /// </summary>
         [Required]
         public virtual DialInfo? dialInfo { get; set; }
     }
--- Source/EvidenceProject/Data/DataModels/DialInfo.cs
+++ Source/EvidenceProject/Data/DataModels/DialInfo.cs
@@ -4,14 +4,24 @@ namespace EvidenceProject.Data.DataModels
 {
     public class DialInfo
     {
+        /// <summary> Unikátní identifikátor kategorie číselníkových záznamů.
+        /// </summary>
         [Key]
         public int id { get; set; }
+
+        /// <summary> Název/obsah kategrie. Max 50 znaků.
+        /// </summary>
         [Required]
         [StringLength(50)]
         public string? name { get; set; }
+
+        /// <summary> Popis kategorie záznamu. Max 200 znaků.
+        /// </summary>
         [StringLength(200)]
         public string? desc { get; set; }
 
+        /// <summary> Obsah kategorie -> stavy v options.
+        /// </summary>
         public virtual List<DialCode>? dialCodes { get; set; }
     }
 }
