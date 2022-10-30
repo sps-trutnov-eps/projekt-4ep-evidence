5cd9f96c6dea81295f5609dd8f491e6dd0afd37f
Jakub Suchánek <suchys.jakub@gmail.com>
2022-10-30 23:57:30 +0100

    Dokumentace FileType

--- Source/EvidenceProject/Data/DataModels/DbFile.cs
+++ Source/EvidenceProject/Data/DataModels/DbFile.cs
@@ -19,6 +19,9 @@ public class DbFile
     [Required]
     public byte[] fileData { get; set; }
     
+    /// <summary>
+    /// Typ souboru
+    /// </summary>
     [Required]
     public string fileType { get; set; }
 }
