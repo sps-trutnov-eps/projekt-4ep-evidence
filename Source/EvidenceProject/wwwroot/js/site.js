afbbc6fd1e39b54ff207285994945f5c4098cbbc
spetlova <spetlova@spstrutnov.cz>
2022-11-03 20:08:52 +0100

    odstraněny console.logy

--- Source/EvidenceProject/wwwroot/js/site.js
+++ Source/EvidenceProject/wwwroot/js/site.js
@@ -48,20 +48,14 @@ function spustitScript(){
     } else if (lokace == "/users/login") {
         loginText();
     }
-    /*} else if (lokace == "/") {
-
-    }*/
 }
 
 function nazvySouboru(){
-    console.log("workin");
     const fileSelector = document.getElementById('photo');
     fileSelector.addEventListener('change', (event) => {
         const fileList = event.target.files;
-        console.log(fileList);
         document.getElementById("nazvy").innerHTML = "";
         for (let i = 0; i < fileList.length; i++) {
-            /*console.log(fileList[i].name);*/
             ted = document.getElementById("nazvy").innerText;
             document.getElementById("nazvy").innerHTML = ted + ", " + fileList[i].name;
 
@@ -70,7 +64,6 @@ function nazvySouboru(){
 }
 
 function loginText() {
-    console.log('logging text');
     $("#login form").submit(function(event) {
         event.preventDefault();
         let formular = $(this);
@@ -96,8 +89,6 @@ function loginText() {
             }
         });
     });
-    console.log('logging text');
-    //e.preventDefault();
 }
 
 function menitHeslo() {
