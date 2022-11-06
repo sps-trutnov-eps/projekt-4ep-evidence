618ba9691071b329ecf4fa84bf3c31eb27489b29
Jakub Suchánek <suchys.jakub@gmail.com>
2022-11-06 21:40:57 +0100

    Code Styling Refractor

--- Source/EvidenceProject/Data/DataModels/DbFile.cs
+++ Source/EvidenceProject/Data/DataModels/DbFile.cs
@@ -1,6 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Data;
-using Microsoft.Data.SqlClient;
 
 namespace EvidenceProject.Data.DataModels;
 
@@ -8,43 +6,44 @@ public class DbFile
 {
     //Any file - https://stackoverflow.com/questions/2579373/saving-any-file-to-in-the-database-just-convert-it-to-a-byte-array
     //Image - http://www.binaryintellect.net/articles/2f55345c-1fcb-4262-89f4-c4319f95c5bd.aspx
-    [Key]
-    public Guid id { get; set; }
-    
-    /// <summary> Název souboru
+    [Key] public Guid id { get; set; }
+
+    /// <summary>
+    ///     Název souboru
     /// </summary>
     [Required]
     public string fileName { get; set; }
-    
-    /// <summary> Data souboru
+
+    /// <summary>
+    ///     Data souboru
     /// </summary>
     [Required]
     public byte[] fileData { get; set; }
-    
 }
 
 public static class FileExtension
 {
     /// <summary>
-    /// Read file from database
+    ///     Read file from database
     /// </summary>
     /// <returns>
-    /// <see cref="MemoryStream"/> from <see cref="DbFile"/>
+    ///     <see cref="MemoryStream" /> from <see cref="DbFile" />
     /// </returns>
-    public static MemoryStream ReadFile(this DbFile file) {
-        MemoryStream memoryStream = new MemoryStream();
+    public static MemoryStream ReadFile(this DbFile file)
+    {
+        var memoryStream = new MemoryStream();
         memoryStream.Write(file.fileData, 0, file.fileData.Length);
         return memoryStream;
     }
 
     /// <summary>
-    /// Write file to database
+    ///     Write file to database
     /// </summary>
     /// <param name="file"></param>
-    /// <param name="stream"><see cref="MemoryStream"/> to write</param>
-    public static void WriteFile(this DbFile file, MemoryStream stream) {
+    /// <param name="stream"><see cref="MemoryStream" /> to write</param>
+    public static void WriteFile(this DbFile file, MemoryStream stream)
+    {
         file.fileData = new byte[stream.Length];
-        stream.Read(file.fileData, 0, (int)stream.Length);
+        stream.Read(file.fileData, 0, (int) stream.Length);
     }
 }
-
--- Source/EvidenceProject/Data/DataModels/DialCode.cs
+++ Source/EvidenceProject/Data/DataModels/DialCode.cs
@@ -1,59 +1,72 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Drawing;
 
-namespace EvidenceProject.Data.DataModels
+namespace EvidenceProject.Data.DataModels;
+
+public class DialCode
 {
-    public class DialCode
+    /// <summary>
+    ///     Konstruktor vytvoří záznam v číselníku
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="dialInfo"></param>
+    /// <param name="color"></param>
+    /// <param name="description"></param>
+    public DialCode(string name, DialInfo? dialInfo, Color? color = null, string? description = "")
     {
-        /// <summary> Unikátní identifikátor záznamu.
-        /// </summary>
-        [Key]  
-        public int id { get; private set; }
-
-        /// <summary> Název/obsah číselníkového záznamu - max 50 znaků
-        /// </summary>
-        [Required]
-        [StringLength(50)]
-        public string? name { get; set; }
-
-        /// <summary> Barevnost vizualizace záznamu.
-        /// </summary>
-        [NotMapped]
-        public Color? color { get => Color.FromArgb(_color); set => _color = value?.ToArgb() ?? 0; }
-
-        /// <summary> Číselný záznam barvy pro uložení do DB.
-        /// </summary>
-        [Required]
-        private int _color { get; set; }
-
-        /// <summary> Popis záznamu číselníku. Max 200 znaků.
-        /// </summary>
-        [StringLength(200)]
-        public string? description { get; set; }
-
-        /// <summary> Kategorie, číselníků do které záznam zapadá.
-        /// </summary>
-        [Required]
-        public virtual DialInfo? dialInfo { get; set; }
-
-        /// <summary> Konstruktor vytvoří záznam v číselníku
-        /// </summary>
-        /// <param name="name"></param>
-        /// <param name="dialInfo"></param>
-        /// <param name="color"></param>
-        /// <param name="description"></param>
-        public DialCode(string name, DialInfo? dialInfo, Color? color = null, string? description = "")
-        {
-            this.name = name;
-            this.color = color ?? Color.White;
-            this.description = description;
-            this.dialInfo = dialInfo;
-        }
-
-        /// <summary> Prázdný konstruktor záznamu
-        /// </summary>
-        public DialCode() { color = Color.White; }
+        this.name = name;
+        this.color = color ?? Color.White;
+        this.description = description;
+        this.dialInfo = dialInfo;
     }
+
+    /// <summary>
+    ///     Prázdný konstruktor záznamu
+    /// </summary>
+    public DialCode()
+    {
+        color = Color.White;
+    }
+
+    /// <summary>
+    ///     Unikátní identifikátor záznamu.
+    /// </summary>
+    [Key]
+    public int id { get; private set; }
+
+    /// <summary>
+    ///     Název/obsah číselníkového záznamu - max 50 znaků
+    /// </summary>
+    [Required]
+    [StringLength(50)]
+    public string? name { get; set; }
+
+    /// <summary>
+    ///     Barevnost vizualizace záznamu.
+    /// </summary>
+    [NotMapped]
+    public Color? color
+    {
+        get => Color.FromArgb(_color);
+        set => _color = value?.ToArgb() ?? 0;
+    }
+
+    /// <summary>
+    ///     Číselný záznam barvy pro uložení do DB.
+    /// </summary>
+    [Required]
+    private int _color { get; set; }
+
+    /// <summary>
+    ///     Popis záznamu číselníku. Max 200 znaků.
+    /// </summary>
+    [StringLength(200)]
+    public string? description { get; set; }
+
+    /// <summary>
+    ///     Kategorie, číselníků do které záznam zapadá.
+    /// </summary>
+    [Required]
+    public virtual DialInfo? dialInfo { get; set; }
 }
