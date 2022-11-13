cc03b537626f8471c22a8f5ee4b38ee2934b4ab0
Dominik Petráš <petras@spstrutnov.cz>
2022-11-13 16:28:17 +0100

    čekání na search

--- Source/EvidenceProject/Controllers/ProjectsController.cs
+++ Source/EvidenceProject/Controllers/ProjectsController.cs
@@ -69,7 +69,8 @@ public class ProjectController : Controller
         if (searchQuery == string.Empty) return Ok();
         List<Project> projects  = _context?.projects?.ToList().Where(project => project.name.Contains(searchQuery)).ToList();
         if (projects == null) return Json("Nic nenalezeno");
-        // Budeme posílat JSON, ať si to JS užijí :D
+        // Budeme posílat JSON, ať si to JS užijí xD
+        // sleeper
         return Json(projects);
     }
 }
