73f77f1a19096332e422b10e47133eb7ff5cf887
Dominik Petráš <petras@spstrutnov.cz>
2022-11-20 23:42:05 +0100

    ŕ̸̎́͛͜e̸̯̟̞̫͖̬̺̫̺̓̓͜f̵͔̠͕̬̳̪͋̑͜ạ̷̭͆͗̚͝ͅc̵͚̫͐̿̊̽t̵̙̝͙̼͆̑̇̅̃ó̷͔̗̌̾̉̄̕͠r̵̨̨̠̺̬͒̅̿̎͑͆̽̎

--- Source/EvidenceProject/Controllers/UserController.cs
+++ Source/EvidenceProject/Controllers/UserController.cs
@@ -103,10 +103,13 @@ public class UserController : Controller
         if (!UniversalHelper.getLoggedUser(HttpContext, out var userID) && userID != null && userID != "1") return Json("Nejsi přihlášen");
         int.TryParse(userID, out int user_id);
         AuthUser? user = AuthUser.FindUser(_context, user_id);
+
         if (user == null) return Json("Uživatel neexistuje?!");
         if (user.password != bcrypt.HashPassword(data.original_password)) return Json("Špatné heslo");
+
         user.password = bcrypt.HashPassword(data.password);
         _context.SaveChanges();
+
         return Json("Heslo změněno úspěšně!");
     }
 }
