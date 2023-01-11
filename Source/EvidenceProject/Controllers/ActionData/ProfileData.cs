f176a8c8028df00d0dd94cb81cb4bd0453f057cd
Daniel Samek <samek@spstrutnov.cz>
2023-01-11 19:07:27 +0100

    Menu Profil obsahuje věci nesouvisející s profilem #134

--- Source/EvidenceProject/Controllers/ActionData/ProfileData.cs
+++ Source/EvidenceProject/Controllers/ActionData/ProfileData.cs
@@ -3,9 +3,4 @@ public class ProfileData : ModelBase
 {
     public List<Project>? Projects { get; set; }
     public AuthUser? AuthUser { get; set; }
-    /*
-    public List<AuthUser>? Users { get; set; }
-    public List<User>? NonAuthUsers { get; set; }
-    public List<DialInfo>? Categories { get; set; }
-    */
 }
